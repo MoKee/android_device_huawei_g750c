b13e467b11f90cab28d3fcb2f743cdecf9e96db7
xjljian <xjljian@163.com>
2016-12-26 10:39:56 +0800

    Update WiFi config

--- device.mk
+++ device.mk
@@ -205,7 +205,8 @@ PRODUCT_PACKAGES += \
     wpa_supplicant_overlay.conf \
     WCNSS_cfg.dat \
     WCNSS_qcom_cfg.ini \
-    WCNSS_qcom_wlan_nv.bin
+    WCNSS_qcom_wlan_nv.bin \
+    WCNSS_qcom_wlan_load_nv.bin
 
 PRODUCT_PACKAGES += \
     hostapd \
